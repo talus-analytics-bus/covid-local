f57f2ef124d5a4c5a403047cea24e6615d4d29e8
Ryan <rzimmerman@talusanalytics.com>
2020-03-21 21:27:08 -0600

    Housekeeping and minor updates

--- src/index.js
+++ src/index.js
@@ -5,37 +5,36 @@ import 'tippy.js/dist/tippy.css';
 
 // Automatically add expand buttons where necessary
 const expandButton = '<button aria-label="Expand Details" class="expand-button"></button>'
-Array.from(document.querySelectorAll('.expanding-boxes section'))
-    .map(section => {
-        if (section.querySelectorAll('.expand-hider').length > 0) {
-            section.children[0].innerHTML += expandButton
-        }
-    })
+document.querySelectorAll('.expanding-boxes section').forEach(section => {
+    if (section.querySelectorAll('.expand-hider').length > 0) {
+        section.children[0].innerHTML += expandButton;
+    };
+});
 
 // Toggle expanding sections
 const animDuration = 500;
 document.querySelectorAll('.expand-hider, .read-more-hider').forEach(element => {
-    element.style.transition = `${animDuration}ms ease`
-})
+    element.style.transition = `${animDuration}ms ease`;
+});
 
 
 let animEnd = undefined;
 const toggleExpandHider = (hider, button) => {
-    const content = hider.children[0]
+    const content = hider.children[0];
     clearTimeout(animEnd);
 
     if (button.getAttribute('aria-label') === "Expand Details") {
 
-        button.setAttribute('aria-label', 'Collapse Details')
+        button.setAttribute('aria-label', 'Collapse Details');
         hider.hidden = false;
         hider.style.height = content.offsetHeight + 'px';
 
         animEnd = setTimeout(() => {
             hider.style.height = 'auto';
-        }, animDuration)
+        }, animDuration);
 
     } else {
-        button.setAttribute('aria-label', 'Expand Details')
+        button.setAttribute('aria-label', 'Expand Details');
         hider.style.height = content.offsetHeight + 'px';
 
         // setTimeout zero to kick it to the end of the callback queue...
@@ -44,25 +43,27 @@ const toggleExpandHider = (hider, button) => {
             // window.requestAnimationFrame(() => {
                 hider.style.height = '0px';
             // });
-        }, 0)
+        }, 0);
 
         animEnd = setTimeout(() => {
             hider.hidden = true;
-        }, animDuration)
-    }
-}
+        }, animDuration);
+    };
+};
 
 // Hide / show the sticky nav... this needs to be improved
 const toggleDocumentNav = () => {
-    const documentNav = document.querySelector('.document-nav')
+    const documentNav = document.querySelector('.document-nav');
+
     documentNav.style.height === '0px'
         ? documentNav.style.height = '5rem'
         : documentNav.style.height = '0px';	
-}
+};
 
 // Update sticky navbar
 const updateDocumentNav = (number) => {
-    const documentNav = document.querySelector('.document-nav')
+    const documentNav = document.querySelector('.document-nav');
+
     if (number) {
         documentNav.innerHTML = 
             `<div class="nav-squish">
@@ -79,86 +80,87 @@ const updateDocumentNav = (number) => {
 
 
 // Expand button click handler
-Array.from(document.querySelectorAll('.expand-button'))
-    .map(button => {
-        button.addEventListener('click', event => {
-            event.stopPropagation();
-            const button = event.target;
-            const hider = button.parentElement.parentElement.children[1]
-            toggleExpandHider(hider, button)
-        })
+document.querySelectorAll('.expand-button').forEach(button => {
+    button.addEventListener('click', event => {
+        event.stopPropagation();
+
+        const button = event.target;
+        const hider = button.parentElement.parentElement.children[1];
+
+        toggleExpandHider(hider, button)
     })
+});
 
 
 // first section click handler
-Array.from(document.querySelectorAll('.first-section'))
-    .map(section => {
-        section.addEventListener('click', event => {
-            const hider = event.target.parentElement.querySelector('.expand-hider')
-                ? event.target.parentElement.querySelector('.expand-hider')
-                : event.target.parentElement.parentElement.querySelector('.expand-hider')
+document.querySelectorAll('.first-section').forEach(section => {
+    section.addEventListener('click', event => {
+        const hider = event.target.parentElement.querySelector('.expand-hider')
+            ? event.target.parentElement.querySelector('.expand-hider')
+            : event.target.parentElement.parentElement.querySelector('.expand-hider');
 
-            const button = hider.parentElement.querySelector('.expand-button')
-            toggleExpandHider(hider, button)
-        })
+        const button = hider.parentElement.querySelector('.expand-button');
+
+        toggleExpandHider(hider, button)
     })
+});
 
 
 // Read More button click handler
-Array.from(document.querySelectorAll('.read-more-button'))
-    .map(button => {
-        button.addEventListener('click', event => {
-            event.stopPropagation();
-            const button = event.target;
-            const hider = button.parentElement.children[1]
-            toggleExpandHider(hider, button)
-        })
+document.querySelectorAll('.read-more-button').forEach(button => {
+    button.addEventListener('click', event => {
+        event.stopPropagation();
+
+        const button = event.target;
+        const hider = button.parentElement.children[1];
+
+        toggleExpandHider(hider, button);
     })
+});
 
 
 
 // Expand correct section when toc link is clicked
-Array.from(document.querySelectorAll('.toc a'))
-    .map(link => {
-        link.addEventListener('click', event => {
+document.querySelectorAll('.toc a').forEach(link => {
+    link.addEventListener('click', event => {
 
-            const anchorID = event.target.getAttribute('href')
-                ? event.target.getAttribute('href')
-                : event.target.parentElement.getAttribute('href')
+        const anchorID = event.target.getAttribute('href')
+            ? event.target.getAttribute('href')
+            : event.target.parentElement.getAttribute('href');
 
-            const section = document.querySelector(anchorID).parentElement
-            const button = section.querySelector('.expand-button')
-            const hider = section.parentElement.querySelector('.expand-hider')
+        const section = document.querySelector(anchorID).parentElement;
+        const button = section.querySelector('.expand-button');
+        const hider = section.parentElement.querySelector('.expand-hider');
 
-            // Not using the toggle handler so that we skip the animation
-            hider.style.height = 'auto'
-            hider.hidden = false;
-            button.setAttribute('aria-label', 'Collapse Details')
+        // Not using the toggle handler so to skip the animation
+        hider.style.height = 'auto';
+        hider.hidden = false;
+        button.setAttribute('aria-label', 'Collapse Details');
 
-            // This is a horrible horrible hack to deal with 
-            // the intersectionObserver only using the top of 
-            // the window, not the top of the nav
-            setTimeout(() => {
-                updateDocumentNav(anchorID.split('-')[1]);
-            }, 100)
-        })
-    })
+        // This is a horrible horrible hack to deal with 
+        // the intersectionObserver only using the top of 
+        // the window, not the top of the nav...
+        setTimeout(() => {
+            updateDocumentNav(anchorID.split('-')[1]);
+        }, 100);
+    });
+});
 
 
 // Parse #action- urls on page load so that people can send
 // links to one another and the right section will open
 if (window.location.hash.includes('action')) {
-    const anchorID = window.location.hash;
 
-    const anchor = document.querySelector(anchorID)
-    const section = anchor.parentElement
-    const button = section.querySelector('.expand-button')
-    const hider = section.parentElement.querySelector('.expand-hider')
+    const anchorID = window.location.hash;
+    const anchor = document.querySelector(anchorID);
+    const section = anchor.parentElement;
+    const button = section.querySelector('.expand-button');
+    const hider = section.parentElement.querySelector('.expand-hider');
 
     // Not using the toggle handler so that we skip the animation
-    hider.style.height = 'auto'
+    hider.style.height = 'auto';
     hider.hidden = false;
-    button.setAttribute('aria-label', 'Collapse Details')
+    button.setAttribute('aria-label', 'Collapse Details');
 
     // This is a horrible hack to deal with 
     // the intersectionObserver only using the top of 
@@ -173,26 +175,25 @@ if (window.location.hash.includes('action')) {
         if (expandBoxesRect.top < 0) {
         toggleDocumentNav();
     }
-    }, 100)
-}
+    }, 100);
+};
 
 // fake "checkbox" click handler
-Array.from(document.querySelectorAll('.expand-area li'))
-    .map(item => {
-        item.addEventListener('click', event => {
-            const rect = event.target.getBoundingClientRect();
-            const elemX = event.clientX - rect.left;
-            if (elemX < -20) {
-                // need to add modal popup
-                alert('To fill out the checklist, please '+ 
-                    'download the printable version at the '+
-                    'top of this page.')
-                    // 'download it here: (this alert is a '+
-                    // 'placeholder for a modal popup I will'+
-                    // ' build in a bit...)')
-            }
-        })
-    })
+document.querySelectorAll('.expand-area li').forEach(item => {
+    item.addEventListener('click', event => {
+        const rect = event.target.getBoundingClientRect();
+        const elemX = event.clientX - rect.left;
+        if (elemX < -20) {
+            // need to add modal popup
+            alert('To fill out the checklist, please '+ 
+                'download the printable version at the '+
+                'top of this page.')
+                // 'download it here: (this alert is a '+
+                // 'placeholder for a modal popup I will'+
+                // ' build in a bit...)')
+        };
+    });
+});
 
 
 // calculate which sections are onscreen any time an intersection 
@@ -219,10 +220,11 @@ const sectionObserver = new IntersectionObserver((entries, observer) => {
         .sort();
 
     updateDocumentNav(onScreen[0]);
-})
+});
 
-Array.from(document.querySelectorAll('.expanding-boxes section'))
-    .map(section => { sectionObserver.observe(section) });
+document.querySelectorAll('.expanding-boxes section').forEach(section => { 
+    sectionObserver.observe(section) 
+});
 
 
 // // This isn't safe, due to asychronous event handling...
@@ -230,30 +232,31 @@ Array.from(document.querySelectorAll('.expanding-boxes section'))
 // 	return Array.from(document.querySelectorAll('.expand-button'))
 // 		.filter(button => button
 // 			.getAttribute('aria-label') === 'Collapse Details')
-// 			.length < 1
-// }
+// 			.length < 1;
+// };
 
 
 // open the document nav after the first section has passed
 const headerObserver = new IntersectionObserver((entries, observer) => {
-    toggleDocumentNav()
+    toggleDocumentNav();
 });
 
-headerObserver.observe(document.querySelector('.introduction-header'))
+headerObserver.observe(document.querySelector('.introduction-header'));
 
-Array.from(document.querySelectorAll('.implementation-metrics input'))
-    .map(input => input.addEventListener('click', event => {
+document.querySelectorAll('.implementation-metrics input').forEach(input => {
+    input.addEventListener('click', event => {
 
-        const results = Array.from(document.querySelectorAll('.implementation-metrics input:first-of-type'))
-        const sum = results.map(input => input.checked ? 1 : 0).reduce((a, b) => a + b)
-        const percent = sum / results.length * 100
+        const results = Array.from(document.querySelectorAll('.implementation-metrics input:first-of-type'));
+        const sum = results.map(input => input.checked ? 1 : 0).reduce((a, b) => a + b);
+        const percent = sum / results.length * 100;
 
         document.querySelector('.implementation-metrics h3')
-            .innerHTML = `Progress: ${percent.toFixed()}%`
-    }))
+            .innerHTML = `Progress: ${percent.toFixed()}%`;
+    });
+});
 
 
-
+// Activate tooltip library
 tippy('[data-tippy-content]', {
     allowHTML: true,
     maxWidth: '65rem',
