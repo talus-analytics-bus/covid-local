2d5375560edb125e61c359bf2596c5dc95170d4c
Ryan <rzimmerman@talusanalytics.com>
2020-05-01 12:25:39 -0600

    New styling, error messages, required fields, submit button labels, dropdown options, wording for contact form

--- covid-local-gatsby/src/pages/contact.js
+++ covid-local-gatsby/src/pages/contact.js
@@ -6,20 +6,47 @@ import Layout from '../components/Layout/Layout'
 import styles from '../styles/contact.module.scss'
 
 const Contact = () => {
-  const [message, setMessage] = React.useState('')
+  const [successMessage, setSuccessMessage] = React.useState('')
+  const [errorMessage, setErrorMessage] = React.useState('')
+  const [submitButtonText, setSubmitButtonText] = React.useState('Submit')
+  const [type, setType] = React.useState('')
 
   const handleSubmit = event => {
     event.preventDefault()
-    const data = new FormData(event.target)
+    const data = Object.fromEntries(new FormData(event.target))
 
-    axios
-      .post(
-        'https://jcvp6imvv9.execute-api.us-east-1.amazonaws.com/submit',
-        JSON.stringify(Object.fromEntries(data)),
-        { headers: { 'Content-Type': 'application/json' } }
+    const emptyfields = Object.values(data).some(field => field === '')
+
+    if (emptyfields) {
+      setErrorMessage(
+        <h2 className={styles.error}>
+          Some required fields below are not filled in, please complete before
+          submitting.
+        </h2>
       )
-      .then(() => setMessage('Feedback Submitted'))
-      .catch(error => console.log(error))
+    } else {
+      setErrorMessage('')
+      setSubmitButtonText('Submitting...')
+      axios
+        .post(
+          'https://jcvp6imvv9.execute-api.us-east-1.amazonaws.com/submit',
+          JSON.stringify(data),
+          { headers: { 'Content-Type': 'application/json' } }
+        )
+        .then(() => {
+          setSuccessMessage(
+            <h2 className={styles.success}>Feedback Submitted</h2>
+          )
+          setSubmitButtonText('Submitting...')
+        })
+        .catch(error =>
+          setErrorMessage(
+            'There was an error submitting your request, please check your network connection and try again.'
+          )
+        )
+    }
+
+    //
   }
 
   return (
@@ -41,52 +68,84 @@ const Contact = () => {
             information via this form.
           </p>
         </div>
+        <div className={styles.formRow}>{errorMessage}</div>
         <div className={styles.formRow}>
           <label className={styles.firstName} htmlFor="firstName">
-            First Name
+            First Name*
             <input type="text" id="firstName" name="firstName" />
           </label>
           <label className={styles.lastName} htmlFor="lastName">
-            Last Name
+            Last Name*
             <input type="text" id="lastName" name="lastName" />
           </label>
         </div>
 
         <div className={styles.formRow}>
           <label className={styles.email} htmlFor="email">
-            Email
+            Email*
             <input type="text" id="email" name="email" />
           </label>
           <label className={styles.org} htmlFor="org">
-            Organization
+            Organization*
             <input type="text" id="org" name="org" />
           </label>
         </div>
 
         <div className={styles.formRow}>
           <label className={styles.type} htmlFor="type">
-            Type of comment
-            <select name="type">
-              <option value="contact">Contact us</option>
-              <option value="feedback">Feedback on the guide</option>
-              <option value="question">Submit comment</option>
+            Topic*
+            <select
+              name="type"
+              onChange={e => {
+                setType(e.target.value)
+                console.log(e.target.value)
+              }}
+            >
+              <option value="General comment">General comment</option>
+              <option value="Feedback on the Guide">
+                Feedback on the Guide
+              </option>
+              <option value="Submit question">Submit question</option>
+            </select>
+          </label>
+
+          <label
+            className={styles.type}
+            htmlFor="type"
+            style={
+              type === 'comment'
+                ? { visibility: 'visible' }
+                : { visibility: 'hidden' }
+            }
+          >
+            Comment category*
+            <select name="category">
+              <option value="General">General</option>
+              <option value="Medical Capacity">Medical Capacity</option>
+              <option value="Logistics">Logistics / PPE Suppy Chain</option>
+              <option value="Testing, Contact Tracing, Surveilance">
+                Disease Testing, Contact Tracing, & Surveilance
+              </option>
+              <option value="Modeling">Modeling</option>
+              <option value="Vulnerable Populations, Low Resource Settings">
+                Vulnerable Populations, Low Resource Settings
+              </option>
+              <option value="Other">Other</option>
             </select>
           </label>
         </div>
 
         <div className={styles.formRow}>
           <label className={styles.body} htmlFor="body">
-            Comment or question
-            <textarea type="text" id="body" name="body" />
-            <p>500 Characters</p>
+            Comment or question*
+            <textarea type="text" id="body" name="body" maxLength="1200" />
+            <p>200 Words</p>
           </label>
         </div>
         <div className={styles.formRow}>
-          <button className={styles.submit}>Submit</button>
+          <button className={styles.submit}>{submitButtonText}</button>
         </div>
-        <div className={styles.formRow}>
-          <p>{message}</p>
-        </div>
+        <div className={styles.formRow}>{successMessage}</div>
       </form>
     </Layout>
   )
