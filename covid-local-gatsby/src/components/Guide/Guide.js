579b96a24c040d4dc1d53bbc1e6afe5effd104f7
Ryan <rzimmerman@talusanalytics.com>
2020-06-10 16:25:37 -0600

    Guide content reversing fixed, added checkboxes and layout

---
+++ covid-local-gatsby/src/components/Guide/Guide.js
@@ -0,0 +1,311 @@
+import React from 'react'
+
+import BlueExpandBox from '../BlueExpandBox/BlueExpandBox'
+
+import styles from './Guide.module.scss'
+
+const CheckboxState = React.createContext({})
+
+const alpha = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+]
+
+const GuideObjective = props => (
+  <BlueExpandBox>
+    <header>
+      <div className={styles.number} data-number={props.objectiveNumber}>
+        {props.objectiveNumber + 1}
+      </div>
+      <h2>{props.objective}</h2>
+      <h3>{props.metadata.title}</h3>
+    </header>
+    <div className={styles.content}>
+      <p>{props.metadata.description}</p>
+      <div className={styles.questions}>{props.children}</div>
+    </div>
+  </BlueExpandBox>
+)
+
+const PriorityActions = props => (
+  <div className={styles.priorityActions}>
+    <h3>{props.section}</h3>
+    <ul>{props.children}</ul>
+  </div>
+)
+
+const GuideAction = props => (
+  <li>
+    {props.question}
+    {props.children}
+  </li>
+)
+
+const OperationalRequirements = props => (
+  <div className={styles.operationalRequirements}>
+    <h3>{props.section}</h3>
+    {props.children}
+  </div>
+)
+
+const AdditionalConsiderations = props => (
+  <div className={styles.additionalConsiderations}>
+    <h3>{props.section}</h3>
+    {props.children}
+  </div>
+)
+
+const GuideRequirement = props => {
+  const checkboxState = React.useContext(CheckboxState)
+  return (
+    <>
+      {props.subquestions.length > 0 ? (
+        <>
+          <label>
+            <span className={styles.checkbox}></span>
+            <span className={styles.number}>{props.questionNumber + 1}. </span>
+            <span className={styles.labelText}>{props.question}</span>
+          </label>
+          <div className={styles.subquestions}>
+            {props.subquestions.map((subquestion, subquestionNumber) => (
+              <GuideSubQuestion
+                {...{ subquestion, subquestionNumber }}
+                id={props.id + subquestionNumber}
+                key={subquestionNumber}
+              />
+            ))}
+          </div>
+        </>
+      ) : (
+        <label>
+          <span className={styles.checkbox}>
+            <input
+              name={props.id}
+              type="checkbox"
+              checked={checkboxState.checkboxStatus[props.id]}
+              onChange={e => checkboxState.changeCheckboxStatus(props.id, e)}
+            />
+          </span>
+          <span className={styles.number}>{props.questionNumber + 1}. </span>
+          <span className={styles.labelText}>{props.question}</span>
+        </label>
+      )}
+    </>
+  )
+}
+
+const GuideSubQuestion = props => {
+  const checkboxState = React.useContext(CheckboxState)
+  return (
+    <label>
+      <span className={styles.checkbox}>
+        <input
+          name={props.id}
+          type="checkbox"
+          checked={checkboxState.checkboxStatus[props.id]}
+          onChange={e => checkboxState.changeCheckboxStatus(props.id, e)}
+        />
+      </span>
+      <span className={styles.number}>{alpha[props.subquestionNumber]}. </span>
+      <span className={styles.labelText}>{props.subquestion}</span>
+    </label>
+    // <label>
+    //   <input
+    //     name={props.id}
+    //     type="checkbox"
+    //     checked={checkboxState.checkboxStatus[props.id]}
+    //     onChange={e => checkboxState.changeCheckboxStatus(props.id, e)}
+    //   />
+    //   {props.subquestionNumber + 1} {props.subquestion}
+    // </label>
+  )
+}
+
+const GuideSection = props => (
+  <div>
+    <h3>{props.section}</h3>
+    <ol>{props.children}</ol>
+  </div>
+)
+
+const GuideQuestion = props => (
+  <li>
+    {props.question}
+    {props.children}
+  </li>
+)
+
+const Guide = props => {
+  // set up in itial checkbox state
+  const checkboxStatusSetup = {}
+  Object.entries(props.content).map(([, metadata], objectiveNumber) => {
+    Object.entries(metadata.sections).map(([, questions], sectionNumber) => {
+      Object.entries(questions).map(([, subquestions], questionNumber) => {
+        if (subquestions.length === 0) {
+          checkboxStatusSetup[
+            '' + objectiveNumber + sectionNumber + questionNumber
+          ] = false
+        }
+
+        subquestions.map((subquestion, subquestionNumber) => {
+          checkboxStatusSetup[
+            '' +
+              objectiveNumber +
+              sectionNumber +
+              questionNumber +
+              subquestionNumber
+          ] = false
+        })
+      })
+    })
+  })
+
+  // checkbox state
+  const [checkboxStatus, setCheckboxStatus] = React.useState(
+    checkboxStatusSetup
+  )
+
+  // load previous checkbox state from storage if it exists
+  React.useEffect(() => {
+    if (window.localStorage.getItem('intlGuideCheckboxes') !== null) {
+      setCheckboxStatus(
+        JSON.parse(window.localStorage.getItem('intlGuideCheckboxes'))
+      )
+    }
+  }, [])
+
+  // update state and save to localstorage
+  const changeCheckboxStatus = (id, e) => {
+    setCheckboxStatus({
+      ...checkboxStatus,
+      [id]: e.target.checked,
+    })
+    console.log('save state' + e.target.checked)
+    window.localStorage.setItem(
+      'intlGuideCheckboxes',
+      JSON.stringify({
+        ...checkboxStatus,
+        [id]: e.target.checked,
+      })
+    )
+  }
+
+  return (
+    <CheckboxState.Provider value={{ checkboxStatus, changeCheckboxStatus }}>
+      <section className={styles.guide}>
+        {// Map over objectives
+        Object.entries(props.content).map(
+          ([objective, metadata], objectiveNumber) => (
+            <GuideObjective
+              key={objectiveNumber}
+              {...{ objective, objectiveNumber, metadata }}
+            >
+              {// Map over sections
+              Object.entries(metadata.sections).map(
+                ([section, questions], sectionNumber) => (
+                  <React.Fragment key={sectionNumber}>
+                    {
+                      {
+                        // Render each section based on the section name
+                        // by matching the section name to this object.
+                        // Writing this all in the render function was probably
+                        // not the best approach, should split logic and content better...
+                        'Priority Actions': (
+                          <PriorityActions {...{ section }}>
+                            {// Map over questions
+                            Object.entries(questions).map(
+                              (question, questionNumber) => (
+                                <GuideAction
+                                  {...{ question, questionNumber }}
+                                  key={questionNumber}
+                                  id={
+                                    '' +
+                                    objectiveNumber +
+                                    sectionNumber +
+                                    questionNumber
+                                  }
+                                ></GuideAction>
+                              )
+                            )}
+                          </PriorityActions>
+                        ),
+                        'Operational Requirements': (
+                          <OperationalRequirements
+                            {...{ section }}
+                            key={'' + objectiveNumber + sectionNumber}
+                          >
+                            {// Map over questions
+                            Object.entries(questions).map(
+                              ([question, subquestions], questionNumber) => (
+                                <GuideRequirement
+                                  {...{
+                                    question,
+                                    questionNumber,
+                                    subquestions,
+                                  }}
+                                  key={questionNumber}
+                                  id={
+                                    '' +
+                                    objectiveNumber +
+                                    sectionNumber +
+                                    questionNumber
+                                  }
+                                />
+                              )
+                            )}
+                          </OperationalRequirements>
+                        ),
+                        'Additional Considerations': (
+                          <AdditionalConsiderations
+                            {...{ section }}
+                            key={'' + objectiveNumber + sectionNumber}
+                          >
+                            {// Map over questions
+                            Object.entries(questions).map(
+                              ([question, subquestions], questionNumber) => (
+                                <GuideRequirement
+                                  {...{
+                                    question,
+                                    questionNumber,
+                                    subquestions,
+                                  }}
+                                  key={questionNumber}
+                                  id={
+                                    '' +
+                                    objectiveNumber +
+                                    sectionNumber +
+                                    questionNumber
+                                  }
+                                />
+                              )
+                            )}
+                          </AdditionalConsiderations>
+                        ),
+                      }[section]
+                    }
+                  </React.Fragment>
+                )
+              )}
+            </GuideObjective>
+          )
+        )}
+      </section>
+    </CheckboxState.Provider>
+  )
+}
+
+export default Guide
--- covid-local-gatsby/src/pages/intl-guide.js
+++ covid-local-gatsby/src/pages/intl-guide.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { useStaticQuery, graphql, Link } from 'gatsby'
 
 import Layout from '../components/Layout/Layout'
-import BlueExpandBox from '../components/BlueExpandBox/BlueExpandBox'
+// import BlueExpandBox from '../components/BlueExpandBox/BlueExpandBox'
+import Guide from '../components/Guide/Guide'
 
 import styles from '../styles/intlGuide.module.scss'
 
@@ -44,7 +45,10 @@ const LmicGuide = () => {
     `
   )
 
-  questions.reverse()
+  // For some reason, the records come in from airtable in reverse order...
+  if (questions[0].node.table !== 'Key Objective #1') {
+    questions.reverse()
+  }
 
   const guideRestructured = {}
   let objective = ''
@@ -58,12 +62,12 @@ const LmicGuide = () => {
     } = edge
 
     // create new objective object
-    if (table !== objective) {
+    if (table.trim() !== objective) {
       const metadata = text.filter(entry =>
         entry.node.data.objective.includes(table)
       )[0].node.data
 
-      objective = table
+      objective = table.trim()
       guideRestructured[objective] = {
         sections: {},
         title: metadata.title,
@@ -72,25 +76,25 @@ const LmicGuide = () => {
     }
 
     // create new section object
-    if (data.Section !== section) {
-      section = data.Section
+    if (data.Section.trim() !== section) {
+      section = data.Section.trim()
       guideRestructured[objective].sections[section] = {}
     }
 
     // create new guiding question, with an array for subquestions
-    if (data.Guiding_Question !== guidingQuestion) {
-      guidingQuestion = data.Guiding_Question
+    if (data.Guiding_Question.trim() !== guidingQuestion) {
+      guidingQuestion = data.Guiding_Question.trim()
       guideRestructured[objective].sections[section][guidingQuestion] = []
     }
 
     // create new subquestion in the array
     if (
-      (data.Associated_subquestions !== subquestion) &
-      (data.Associated_subquestions !== 'N/A')
+      (data.Associated_subquestions.trim() !== subquestion) &
+      (data.Associated_subquestions.trim() !== 'N/A')
     ) {
-      subquestion = data.Associated_subquestions
+      subquestion = data.Associated_subquestions.trim()
       guideRestructured[objective].sections[section][guidingQuestion].push(
-        data.Associated_subquestions
+        subquestion
       )
     }
   })
@@ -104,44 +108,7 @@ const LmicGuide = () => {
         <Link to="/contact/">Contact Us</Link>
       </header>
       <section className={styles.main}>
-        <section className={styles.guide}>
-          {Object.entries(guideRestructured).map(([objective, metadata]) => (
-            <BlueExpandBox key={objective}>
-              <header>
-                <h2>{objective}</h2>
-                <h3>{metadata.title}</h3>
-              </header>
-              <div className={styles.content}>
-                <div className={styles.text}>
-                  <p>{metadata.description}</p>
-                  {Object.entries(metadata.sections).map(
-                    ([section, questions]) => (
-                      <div key={section}>
-                        <h3>{section}</h3>
-                        <ol>
-                          {Object.entries(questions).map(
-                            ([question, subquestions]) => (
-                              <li key={question}>
-                                {question}
-                                {subquestions.length > 0 && (
-                                  <ol>
-                                    {subquestions.map(subquestion => (
-                                      <li key={subquestion}>{subquestion}</li>
-                                    ))}
-                                  </ol>
-                                )}
-                              </li>
-                            )
-                          )}
-                        </ol>
-                      </div>
-                    )
-                  )}
-                </div>
-              </div>
-            </BlueExpandBox>
-          ))}
-        </section>
+        <Guide content={guideRestructured} />
       </section>
     </Layout>
   )
