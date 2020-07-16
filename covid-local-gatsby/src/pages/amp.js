179b7d14e0acd4910a39a2006896e452ee6e1705
Ryan <rzimmerman@talusanalytics.com>
2020-07-16 14:55:12 -0600

    og:meta tags for the amp landing page

--- covid-local-gatsby/src/pages/amp.js
+++ covid-local-gatsby/src/pages/amp.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 import LandingPage from './index.js'
 
 const AmpPage = () => {
-  return <LandingPage ampOpen={true} />
+  return (
+    <>
+      <Helmet title={`COVID-Amp: Analysis and Mapping of Policies`}>
+        <meta
+          name="description"
+          content={
+            'A tool for exploring, analyzing, and mapping COVID-19 policies enacted around the globe.'
+          }
+        />
+        <meta
+          name="image"
+          content={'/images/amp-screenshots/global-policy-maps.png'}
+        />
+        <meta
+          property="og:title"
+          content={'COVID-Amp: Analysis and Mapping of Policies'}
+        />
+        <meta
+          property="og:description"
+          content={
+            'A tool for exploring, analyzing, and mapping COVID-19 policies enacted around the globe.'
+          }
+        />
+        <meta
+          property="og:image"
+          content={'/images/amp-screenshots/global-policy-maps.png'}
+        />
+      </Helmet>
+      <LandingPage ampOpen={true} />
+    </>
+  )
 }
 
 export default AmpPage
